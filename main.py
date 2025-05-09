51725973a213af6414180debd5be89fd7ca43918
LlamaCodrr950 <163625693+ASimpleCoder@users.noreply.github.com>
2025-05-09 09:18:06 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+import smtplib as smtp
+
+sender = smtp.SMTP("smtp.gmail.com", 587)
+sender.ehlo()
+sender.starttls()
+sender.login(st.secrets["email"]["sender"], st.secrets["email"]["apppassword"])
+
+b = st.button("Click Me")
+if b:
+  sender.sendmail("python.user.143@gmail.com", "boycalledutkarsh@gmail.com", "HELLO \n\n Please check if u have recieved this")
